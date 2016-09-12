f2bc7449632b0875663cb0aa6b51b4afeb0c33ba
Stephanie <stephaniecain@Stephanies-MacBook-Pro.local>
2016-09-11 21:16:47 -0500

    Added font awesome
    For social media icons

--- assets/script/main.js
+++ assets/script/main.js
@@ -1,15 +1,3 @@
-function sortType(val) {
-    $(".project").hide();
-    if (val == "all") {
-      $(".project").show();
-    } else { $("[data-type='" + val + "']").show(); }
-}
-function sortJob(val) {
-    $(".project").hide();
-    if (val == "all") {
-      $(".project").show();
-    } else { $("[data-job='" + val + "']").show(); }
-}
 function menuToggle(e){
   $("#js-centered-navigation-menu").toggleClass("show");
   $("#nav-open, #nav-close").toggle()
